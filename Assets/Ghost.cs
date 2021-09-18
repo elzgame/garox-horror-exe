48907a5ffe1f2740dfcefca6bb795a68d139d326
elzgame <elzgamedev@gmail.com>
2021-09-18 18:41:06 +0700

    NavMeshAgent Solved Everything, simple

--- Assets/Ghost.cs
+++ Assets/Ghost.cs
@@ -1,25 +1,34 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Ghost : MonoBehaviour
 {
     public Camera cam;
     public GameObject player;
     public float speed = 1;
+    private NavMeshAgent agent;
+
+    void Start() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
     void Update()
     {
-        var ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit, 100))
-        {
-            if (hit.transform.tag == "Player")
-            {
-                float step = speed * Time.deltaTime;
-                transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), step);
-                transform.LookAt(player.transform, Vector3.up);
-            }
-        }
+        // var ray = cam.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        // RaycastHit hit;
+        // if (Physics.Raycast(ray, out hit, 100))
+        // {
+        //     if (hit.transform.tag == "Player")
+        //     {
+        //         float step = speed * Time.deltaTime;
+        //         transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), step);
+        //         transform.LookAt(player.transform, Vector3.up);
+        //     }
+        // }
+        // float step = speed * Time.deltaTime;
+        // transform.position = Vector3.MoveTowards(transform.position, new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z), step);
+        // transform.LookAt(player.transform, Vector3.up);
+        agent.SetDestination(new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z));
 
     }
 }
